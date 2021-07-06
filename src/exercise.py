2f982822fbed42463562a94a08e1ae91584d3d3c
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-07-06 10:58:15 +0100

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,7 @@
 def main():
-    #write your code below this line
+    number = int(input('How many days would you like to convert to seconds?: '))
+    result = number * 86400
+    print(result)#write your code below this line
 
 if __name__ == '__main__':
     main()
